d4a4425b5c0d6cf1085bc188cf942fa478c271dd
Priyank Desai <32032494+priyank7191@users.noreply.github.com>
2022-06-01 22:22:30 +0530

    Update db.js
    uri change

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
-        // const conn = await mongoose.connect("mongodb+srv://webdevpriyank:webdevpriyank@priyankcluster.zwfzgdb.mongodb.net/mernapp?retryWrites=true&w=majority")
+//         const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect("mongodb+srv://webdevpriyank:webdevpriyank@priyankcluster.zwfzgdb.mongodb.net/mernapp?retryWrites=true&w=majority")
         console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold)
 
     } catch (err) {
